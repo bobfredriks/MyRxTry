0ee17bc610c2d3e8ac3742541666975b02106262
bobfredriks <bob@microcontrol.cc>
2019-02-25 12:40:17 -0800

    Just test

---
+++ app/src/main/java/com/microcontrolapps/myrxtry/Vehicals.kt
@@ -0,0 +1,9 @@
+package com.microcontrolapps.myrxtry
+
+class Vehicals constructor(val mcolor: String, val mmake: String, val mmodel: String) {
+
+
+}
+
+val Mycar = Vehicals("Blue", "Chevy", "Camaro")
+val adf = Mycar.mcolor
